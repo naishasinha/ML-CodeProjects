0264bd4c9075f46d45156968304077ffbe982d9f
Naisha Sinha <117387359+naishasinha@users.noreply.github.com>
2024-06-13 00:55:40 -0700

    Add files via upload

---
+++ hw1-A/homeworks/poly_regression/polyreg.py
@@ -0,0 +1,154 @@
+"""
+    Template for polynomial regression
+    AUTHOR Eric Eaton, Xiaoxiang Hu
+"""
+
+from typing import Tuple
+
+import numpy as np
+
+from utils import problem
+
+
+class PolynomialRegression:
+    @problem.tag("hw1-A", start_line=5)
+    def __init__(self, degree: int = 1, reg_lambda: float = 1e-8):
+        """Constructor
+        """
+        self.degree: int = degree
+        self.reg_lambda: float = reg_lambda
+        # Fill in with matrix with the correct shape
+        self.weight: np.ndarray = None  # type: ignore
+        # You can add additional fields
+        self.mean = None
+        self.std = None
+
+    @staticmethod
+    @problem.tag("hw1-A")
+    def polyfeatures(X: np.ndarray, degree: int) -> np.ndarray:
+        """
+        Expands the given X into an (n, degree) array of polynomial features of degree degree.
+
+        Args:
+            X (np.ndarray): Array of shape (n, 1).
+            degree (int): Positive integer defining maximum power to include.
+
+        Returns:
+            np.ndarray: A (n, degree) numpy array, with each row comprising of
+                X, X * X, X ** 3, ... up to the degree^th power of X.
+                Note that the returned matrix will not include the zero-th power.
+
+        """
+        polyfeatures_of_X = X
+        X_expand = [X.flatten()]
+        for i in range(1, degree):
+            X_expand.append((X**(i+1)).flatten())
+        polyfeatures_of_X = np.array(X_expand).T
+        return polyfeatures_of_X
+
+    @problem.tag("hw1-A")
+    def fit(self, X: np.ndarray, y: np.ndarray):
+        """
+        Trains the model, and saves learned weight in self.weight
+
+        Args:
+            X (np.ndarray): Array of shape (n, 1) with observations.
+            y (np.ndarray): Array of shape (n, 1) with targets.
+
+        Note:
+            You will need to apply polynomial expansion and data standardization first.
+        """
+        X = self.polyfeatures(X, self.degree)
+        self.std = np.std(X, axis = 0)
+        self.mean = np.mean(X, axis = 0)
+
+        X = (X - self.mean) / self.std
+        X = np.c_[np.ones([len(X), 1]), X]
+
+        n, d = X.shape
+
+        reg_matrix = self.reg_lambda * np.eye(d)
+        reg_matrix[0, 0] = 0
+
+        self.weight = np.linalg.pinv(X.T.dot(X) + reg_matrix).dot(X.T).dot(y)
+
+    @problem.tag("hw1-A")
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Use the trained model to predict values for each instance in X.
+
+        Args:
+            X (np.ndarray): Array of shape (n, 1) with observations.
+
+        Returns:
+            np.ndarray: Array of shape (n, 1) with predictions.
+        """
+        X = self.polyfeatures(X, self.degree)
+        X = (X - self.mean) / self.std
+        X = np.c_[np.ones([len(X), 1]), X]
+        return X.dot(self.weight)
+
+
+@problem.tag("hw1-A")
+def mean_squared_error(a: np.ndarray, b: np.ndarray) -> float:
+    """Given two arrays: a and b, both of shape (n, 1) calculate a mean squared error.
+
+    Args:
+        a (np.ndarray): Array of shape (n, 1)
+        b (np.ndarray): Array of shape (n, 1)
+
+    Returns:
+        float: mean squared error between a and b.
+    """
+    return np.mean((a-b)**2)
+
+
+@problem.tag("hw1-A", start_line=5)
+def learningCurve(
+    Xtrain: np.ndarray,
+    Ytrain: np.ndarray,
+    Xtest: np.ndarray,
+    Ytest: np.ndarray,
+    reg_lambda: float,
+    degree: int,
+) -> Tuple[np.ndarray, np.ndarray]:
+    """Compute learning curves.
+
+    Args:
+        Xtrain (np.ndarray): Training observations, shape: (n, 1)
+        Ytrain (np.ndarray): Training targets, shape: (n, 1)
+        Xtest (np.ndarray): Testing observations, shape: (n, 1)
+        Ytest (np.ndarray): Testing targets, shape: (n, 1)
+        reg_lambda (float): Regularization factor
+        degree (int): Polynomial degree
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray]: Tuple containing:
+            1. errorTrain -- errorTrain[i] is the training mean squared error using model trained by Xtrain[0:(i+1)]
+            2. errorTest -- errorTest[i] is the testing mean squared error using model trained by Xtrain[0:(i+1)]
+
+    Note:
+        - For errorTrain[i] only calculate error on Xtrain[0:(i+1)], since this is the data used for training.
+            THIS DOES NOT APPLY TO errorTest.
+        - errorTrain[0:1] and errorTest[0:1] won't actually matter, since we start displaying the learning curve at n = 2 (or higher)
+    """
+    n = len(Xtrain)
+
+    errorTrain = np.zeros(n)
+    errorTest = np.zeros(n)
+    # Fill in errorTrain and errorTest arrays
+    model = PolynomialRegression(degree = degree, reg_lambda = reg_lambda)
+
+    for i in range (1, n):
+        train_feature = Xtrain[0:(i+1)]
+        train_label = Ytrain[0:(i+1)]
+
+        w = model.fit(train_feature, train_label)
+
+        train_prediction = model.predict(train_feature)
+        test_prediction = model.predict(Xtest)
+
+        errorTrain[i] = mean_squared_error(train_prediction, train_label)
+        errorTest[i] = mean_squared_error(test_prediction, Ytest)
+    return errorTrain, errorTest
+
